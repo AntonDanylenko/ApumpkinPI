cada59e72c79d228008c2eef12d16c0115701b43
zane0916 <zwang7@stuy.edu>
2018-11-28 13:25:50 -0500

    added placeholder methods to app.py

--- app.py
+++ app.py
@@ -3,13 +3,26 @@
 # P01 -- ArRESTed Development
 # 2018-11-28
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
 
 @app.route("/")
 def generate():
     return render_template("homepage.html")
-    
+'''
+@app.route("/login")
+    render_template("login.html")
+
+@app.route("/register")
+    render_template("register.html")
+
+@app.route("/movies")
+    render_template("<>.html")
+
+@app.route("/info")
+    render_template("<>.html")
+'''
+
 if (__name__ == "__main__"):
     app.run()
