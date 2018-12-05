410609fe4b26cd983f25e2c1f13afbdf6ff89dd4
dereksong01 <dsong1@stuy.edu>
2018-12-05 13:59:44 -0500

    NYPL key & buggy search

--- app.py
+++ app.py
@@ -9,19 +9,23 @@ from flask import Flask, render_template, flash, request
 
 app = Flask(__name__)
 
+choice = ""
+search = ""
+
 @app.route("/")
 def generate():
-    return render_template("homepage.html")
 
+    choice = request.form["choice"]
+    search = request.form["search"]
+
+    return render_template("homepage.html")
+'''
 @app.route("/login")
-def login():
-    return render_template("login.html")
+    render_template("login.html")
 
 @app.route("/register")
-def reg():
-    return render_template("register.html")
+    render_template("register.html")
 
-'''
 @app.route("/movies")
     render_template("<>.html")
 
@@ -37,23 +41,29 @@ def search():
     nytkey = "7e297703ad9e4b9595f9d7b9bff79582"
     nyt = ""
 
-    if (request.select['choice'] == "name"):
-        mtitle = "t=" + request.form['search'] # gets the movie title that was searched and formats it for the api to work
+    mdata = {}
+
+    if (choice == "name"):
+        mtitle = "t=" + search # gets the movie title that was searched and formats it for the api to work
         omdburl = omdb + mtitle
         x = urllib.request.urlopen(omdburl).read()
         mdata = json.loads(x)
-        #print(mdata)
-    if (request.select['choice'] == "date"):
-        myear = "y=" + request.form['search'] # gets the year that was searched and formats it
-        omdburl = omdb + myear
-        x = urllib.request.urlopen(omdburl).read()
-        mdata = jason.loads(x)
-    #if (request.select['choice'])
-        args = {}
-        args['title'] = mdata['Title']
-        args['year'] = mdata['Year']
-        args['actors'] = mdata['Actors']
-        args['rating'] = mdata['Ratings']
+        print(mdata)
+
+    #if (choice == "date"):
+    #    myear = "y=" + request.form['search'] # gets the year that was searched and formats it
+    #    omdburl = omdb + myear
+    #    x = urllib.request.urlopen(omdburl).read()
+    #    mdata = json.loads(x)
+
+    #if (choice == "cast"):
+    #if (choice == "genre"):
+    
+    args = {}
+    args['title'] = mdata['Title']
+    args['year'] = mdata['Year']
+    args['actors'] = mdata['Actors']
+    args['rating'] = mdata['Ratings']
 
     return render_template('movie.html', **args)
 
