1de17537e413a48cd9a348059b5beb9869c462a3
dereksong01 <dsong1@stuy.edu>
2018-12-04 22:47:42 -0500

    app d

--- app.py
+++ app.py
@@ -28,20 +28,32 @@ def generate():
 
 @app.route("/search", methods = ["POST", "GET"])
 def search():
+    okey = "b7503b8d"
+    omdb = "http://www.omdbapi.com/?apikey=" + okey + "&"
+
+    nytkey = "7e297703ad9e4b9595f9d7b9bff79582"
+    nyt = ""
+
     if (request.select['choice'] == "name"):
-        key = "b7503b8d"
-        omdb = "http://www.omdbapi.com/?apikey=" + key + "&"
         mtitle = "t=" + request.form['search'] # gets the movie title that was searched and formats it for the api to work
         omdburl = omdb + mtitle
         x = urllib.request.urlopen(omdburl).read()
         mdata = json.loads(x)
         #print(mdata)
+    if (request.select['choice'] == "date"):
+        myear = "y=" + request.form['search'] # gets the year that was searched and formats it
+        omdburl = omdb + myear
+        x = urllib.request.urlopen(omdburl).read()
+        mdata = jason.loads(x)
+    #if (request.select['choice'])
         args = {}
         args['title'] = mdata['Title']
         args['year'] = mdata['Year']
         args['actors'] = mdata['Actors']
         args['rating'] = mdata['Ratings']
+
     return render_template('movie.html', **args)
 
 if (__name__ == "__main__"):
+    app.debug = True
     app.run()
